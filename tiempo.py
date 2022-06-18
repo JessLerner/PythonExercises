c2cea4a4bdd930bb37fa1ad32c023a453429b0f2
Jesica Lerner <102632133+JessLerner@users.noreply.github.com>
2022-06-18 16:54:45 -0300

    Add files via upload
    Crear un script que nos diga si es la hora de ir a casa. Tendréis que hacer uso del modulo time. Necesitaréis la fecha del sistema y poder comprobar la hora. 
    En el caso de que sean más de las 19, se mostrará un mensaje y en caso contrario, haréis una operación para calcular el tiempo que queda de trabajo.
    
    Create a Script that tell us if it's time to go home. We have to make use of the module time. We need the system time and compare the date. In case is pass 19, it will shows a messege that informs that you can go home, otherwise you make a operation to calculate the time left at work.

---
+++ tiempo.py
@@ -0,0 +1,11 @@
+"""Crear un script que nos diga si es la hora de ir a casa. Tendréis que hacer uso del modulo time. Necesitaréis la fecha del sistema y poder comprobar la hora. 
+En el caso de que sean más de las 19, se mostrará un mensaje y en caso contrario, haréis una operación para calcular el tiempo que queda de trabajo."""
+
+import time
+
+if time.localtime().tm_hour>=19 and time.localtime().tm_hour<=8:
+	print("Puedes irte a casa")
+elif time.localtime().tm_hour==18:
+	print("Faltan ",60-time.localtime().tm_min,"minutos para que puedas irte")
+else:
+	print("Faltan ",18-time.localtime().tm_hour,"horas y",58-time.localtime().tm_min," minutos para que puedas irte")
